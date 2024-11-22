aaef29ce772982457e6368fe993ff7880efdd9ac
unknown <sunebarikara@gmail.com>
2024-11-22 23:41:46 +0100

    Resolved File

--- Documents/React/Auth-App/api/index.js
+++ Documents/React/Auth-App/api/index.js
@@ -2,12 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config()
 
 mongoose.connect(process.env.MONGODB).then (() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
-    console.log(err);
+    console.log(err)
 })
 
 const app = express();
